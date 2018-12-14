04c224acf0d3fd64eb152a2c873cf07f46ba6054
CongChinhPham <chinhpham1609@gmail.com>
2018-12-14 22:47:14 +0700

    Update index.php

--- officialforum/libs/PHPMailer/index.php
+++ officialforum/libs/PHPMailer/index.php
@@ -8,8 +8,8 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'aovang5wqe@gmail.com';                 // SMTP username
-$mail->Password = 'Congchinhcity169';                           // SMTP password
+$mail->Username = '';                 // SMTP username
+$mail->Password = '';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
