95196dada975e7409f424fc68c1690b8221197c5
CongChinhPham <chinhpham1609@gmail.com>
2018-12-14 22:59:39 +0700

    Create home.php

---
+++ officialforum/views/pages/home.php
@@ -0,0 +1,6 @@
+<?php 
+	$page_title = 'Trang chủ';
+  	echo "Tên tôi là: $name, năm nay tôi $age tuổi";
+  	echo "<br>";
+  	echo '<a href="index.php?controller=access&action=login">Accept</a>';
+?>
